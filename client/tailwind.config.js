87a79b7565e29f5fe4b81d8d9713ce7fd3d67074
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2021-10-26 22:36:32 +0200

    change default colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,22 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      spacing : {
-        70: '17.5rem'
-      }
+      colors: {
+        blue: {
+          100: "#cce4f6",
+          200: "#99c9ed",
+          300: "#66afe5",
+          400: "#3394dc",
+          500: "#0079d3",
+          600: "#0061a9",
+          700: "#00497f",
+          800: "#003054",
+          900: "#00182a",
+        },
+      },
+      spacing: {
+        70: "17.5rem",
+      },
     },
   },
   variants: {
