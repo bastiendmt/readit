1675b14e42f2632b1fa658e0cf27e790fecb6b4d
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2021-10-27 20:02:03 +0200

    navbar

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,23 @@
-import "../styles/globals.css";
-import { AppProps } from "next/app";
 import Axios from "axios";
+import { AppProps } from "next/app";
+import "../styles/tailwind.css";
+import Navbar from "../components/Navbar";
+import { useRouter } from "next/dist/client/router";
 
 Axios.defaults.baseURL = "http://localhost:5000/api";
 Axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+  const authRoutes = ['/register', '/login']
+  const authRoute = authRoutes.includes(pathname)
+
+  return (
+    <>
+      {!authRoute && <Navbar />}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -21,8 +21,9 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600&display=swap"
             rel="stylesheet"
           />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         </Head>
-        <body className="font-body">
+        <body className="font-body" style={{backgroundColor :'#DAE0E6' }}>
           <Main />
           <NextScript />
         </body>
