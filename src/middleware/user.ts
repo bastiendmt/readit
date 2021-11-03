2cd59384882a0d1d329dba97702e05828a365f77
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2021-11-03 22:15:40 +0100

    fixed user middleware

--- src/middleware/user.ts
+++ src/middleware/user.ts
@@ -1,16 +1,15 @@
 import { NextFunction, Request, Response } from "express";
-import User from "../entities/User";
 import jwt from "jsonwebtoken";
+import User from "../entities/User";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
     const token = req.cookies.token;
-
-    if (!token) return next;
+    if (!token) return next();
 
     const { username }: any = jwt.verify(token, process.env.JWT_SECRET!);
 
-    const user = await User.findOneOrFail({ username });
+    const user = await User.findOne({ username });
 
     res.locals.user = user;
 
