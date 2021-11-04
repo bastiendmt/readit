7f09de569782e009957af56abd92ad133a57c9ef
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2021-11-04 20:52:08 +0100

    cleanup

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,8 @@
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import Axios from "axios";
-import { Post } from "../types";
-import Link from "next/link";
-
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { GetServerSideProps } from "next";
-import PostCard from "../components/PostCard";
-
+import Head from "next/head";
 import useSWR from "swr";
+import PostCard from "../components/PostCard";
 
 dayjs.extend(relativeTime);
 
